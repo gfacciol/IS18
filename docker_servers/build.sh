d6f682d56fcf7462f82d9c6fc6837a6d6bc76625
Gabriele Facciolo <gfacciol@gmail.com>
2018-06-11 15:42:18 +0200

    remove server

--- docker_servers/build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t facciolo/is18-satellite-minitutorial .
--- docker_servers/singleuser_initscript.sh
+++
@@ -1,2 +0,0 @@
-cd /home/student1 && jupyter notebook --port=8000 --ip=* --allow-root --NotebookApp.token='' & 
-cd /shared && http-server -p 8008 
